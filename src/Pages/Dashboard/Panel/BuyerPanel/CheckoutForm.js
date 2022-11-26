4565874a67c742c5fe75eb242d7dbd9e3dc87c9e
A. Z. M. Arif <azmarifcode@gmail.com>
2022-11-27 00:07:20 +0600

    some

--- src/Pages/Dashboard/Panel/BuyerPanel/CheckoutForm.js
+++ src/Pages/Dashboard/Panel/BuyerPanel/CheckoutForm.js
@@ -1,10 +1,29 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 
 const CheckoutForm = ({ paymentData }) => {
     const [cardError, setCardError] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+    const [ transactionId, setTransactionId ] = useState('');
+    const [ stripeSuccess, setStripeSuccess ] = useState(' ');
+        const [stripeId, setStripeId] = useState(' ');
+    const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState('');
     const stripe = useStripe();
     const elements = useElements();
+    const { price, name, email, _id } = paymentData;
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_LOCALHOST}/create-payment-intent`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ price }),
+        })
+            .then((res) => res.json())
+            .then((data) => setClientSecret(data.clientSecret));
+    }, [price]);
+
     const handleSubmit = async (event) => {
         event.preventDefault();
         if (!stripe || !elements) {
@@ -26,6 +45,59 @@ const CheckoutForm = ({ paymentData }) => {
         } else {
             setCardError('');
         }
+        setSuccess('')
+        setLoading(true)
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: name,
+                        email: email
+                    },
+                },
+            },
+        );
+        if(confirmError){
+            setCardError(confirmError.message);
+                return;
+        }
+        if (paymentIntent.status === "succeeded")
+        {
+            setSuccess('congrats! your payment completed')
+            setTransactionId(paymentIntent.id)
+        }
+
+///////////////////////////////////////////////////
+        const payment = {
+            price,
+            email,
+            name,
+            transitionId: paymentIntent.id,
+            bookingId: _id,
+        };
+        if (paymentIntent.status === 'succeeded') {
+            fetch('http://localhost:7000/payments', {
+                method: 'POST',
+                headers: {'content-type': 'application/json'},
+                body: JSON.stringify(payment),
+            })
+                .then((res) => res.json())
+                .then((data) =>
+                {
+                    toast.success('success')
+                    console.log(data);
+                    setStripeSuccess('congrats!, Your payment completed');
+                    setStripeId(paymentIntent.id);
+                });
+        }
+
+////////
+
+
+        setLoading(false)
+        console.log('paymentIntent', paymentIntent);
     };
     return (
         <div>
@@ -49,11 +121,19 @@ const CheckoutForm = ({ paymentData }) => {
                 <button
                     type="submit"
                     className="btn mt-5 btn-primary btn-sm"
-                    disabled={!stripe}>
+                    disabled={!stripe || !clientSecret || loading}>
                     Pay
                 </button>
             </form>
             <div className="text-red-600"> {cardError}</div>
+            {success && (
+                <div>
+                    <p className="text-green-600">{success}</p>
+                    <p>
+                       Your transactionId: <span className="font-bold">{transactionId}</span>
+                    </p>
+                </div>
+            )}
         </div>
     );
 };
